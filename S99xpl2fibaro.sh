48dd01e99c330bfd0411fa419b87f9b19b365b70
Eremid <eremid@users.noreply.github.com>
2014-04-08 23:48:34 +0200

    Add init script for synology NAS

---
+++ S99xpl2fibaro.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DAEMON=/volume1/web/pyhon/xpl2fibaro.py
+
+case "$1" in
+    start)
+	$DAEMON start > /dev/null 2>&1
+	;;
+
+    stop)
+	$DAEMON stop > /dev/null 2>&1
+	;;
+
+    restart)
+	$DAEMON restart > /dev/null 2>&1
+	;;
+
+    *)
+	echo "Usage: S99xpl2fibaro.sh start|stop|restart"
+	;;
+esac
